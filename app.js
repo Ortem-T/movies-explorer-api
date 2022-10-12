36b36eb9f40b2c326da5209d5e491e5ecb5a2e9f
Ortem-T <84287305+Ortem-T@users.noreply.github.com>
2022-10-12 15:28:59 +0300

    feat: cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const { PORT = 3000, DB = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const app = express();
@@ -9,6 +10,17 @@ const handleError = require('./errors/handleError');
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
 app.use(express.json());
 
 mongoose.connect(DB, {
@@ -16,6 +28,7 @@ mongoose.connect(DB, {
 });
 
 app.use(requestLogger);
+app.use('*', cors(options));
 
 app.use(routes);
 
